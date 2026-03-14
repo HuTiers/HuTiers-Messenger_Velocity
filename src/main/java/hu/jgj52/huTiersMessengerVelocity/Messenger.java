b3fefe8e4863cdb28fbb8cbe8516a6fef146d731
jgj52 <email@jgj52.hu>
2026-03-14 20:49:16 +0100

    i should not release it right away before testing

--- src/main/java/hu/jgj52/huTiersMessengerVelocity/Messenger.java
+++ src/main/java/hu/jgj52/huTiersMessengerVelocity/Messenger.java
@@ -2,21 +2,51 @@ package hu.jgj52.huTiersMessengerVelocity;
 
 import redis.clients.jedis.JedisPubSub;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 import static hu.jgj52.huTiersMessengerVelocity.HuTiersMessengerVelocity.jedis;
 
 public class Messenger {
+    private static final ExecutorService thread = Executors.newSingleThreadExecutor(r -> new Thread(r, "hutiers-manager"));
+
+
     public static void listen(String channel, Consumer<String> consumer) {
-        new Thread(() -> jedis.subscribe(new JedisPubSub() {
-            @Override
-            public void onMessage(String channel, String message) {
-                consumer.accept(message);
+        thread.submit(() -> {
+            if (jedis == null) {
+                try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
+                listen(channel, consumer);
+                return;
             }
-        }, channel)).start();
+
+            try {
+                jedis.subscribe(new JedisPubSub() {
+                    @Override
+                    public void onMessage(String ch, String message) {
+                        consumer.accept(message);
+                    }
+                }, channel);
+            } catch (Exception e) {
+                e.printStackTrace();
+                try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
+            }
+        });
     }
 
     public static void send(String channel, String message) {
-        new Thread(() -> jedis.publish(channel, message));
+        thread.submit(() -> {
+            if (jedis == null) {
+                try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
+                send(channel, message);
+                return;
+            }
+
+            try {
+                jedis.publish(channel, message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
